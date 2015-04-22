a84d9e4c188185e8b4cd722465da32163d7fbcee
David Chen <chenwei2000121@qq.com>
2015-04-22 17:58:20 +0800

    new layout and method

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var labelResult: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +22,12 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func btnTapped(sender: ZFRippleButton) {
+        labelResult.text = sender.titleForState(.Normal)
+    }
 
+    @IBAction func operatorTapped(sender: ZFRippleButton) {
+        //
+    }
 }
 
